c8100a95048035730ea42d489ca58f5e2259a352
BlountIV <iv.mosesblount@gmail.com>
2022-12-04 19:24:25 -0500

    test commit

--- server.js
+++ server.js
@@ -2,13 +2,21 @@ const express = require ('express');
 const app = express();
 
 // routes
-app.get('/Greeting/:name', function(req, res) {
+app.get('/Greeting/:name', (req, res) => {
     res.send("What's good" + req.params.name + "its good to see you!");
 });
 
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+    const customerSatisfaction = (req.params.total * (req.params.tipPercentage / 100))
+   res.send('Your tip is' + ' ' + customerSatisfaction); 
+});
 
 
+app.get('/magic/:phrase', (req, res) => {
+    const phrases = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
 
+    let guidance = phrases [Math.floor(Math.random() * phrases.length)]
+    res.send(req.params.phrase + `<h1>${guidance}</h1>`);
 
 
 
